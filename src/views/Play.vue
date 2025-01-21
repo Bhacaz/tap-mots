5a71db728ef053feecc73e15534e702cb0d46dca
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-01-20 20:49:43 -0500

    currentWord

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import {ref} from "vue";
 
+const words = ref([]);
+const currentWord = ref('');
+const storedItems = localStorage.getItem('words');
+if (storedItems) {
+  words.value = JSON.parse(storedItems);
+}
+
+const i = Math.floor(Math.random() * words.value.length);
+currentWord.value = words.value[i]
 </script>
 
 <template>
- <h1>Play</h1>
+  <h1>{{words.join(';')}}</h1>
+  <h1>{{currentWord}}</h1>
 </template>
 
 <style scoped>
